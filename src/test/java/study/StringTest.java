20067b72bbfd31b4532fea5182953df90ec2e800
jaejoon <dkansk924@naver.com>
2021-05-28 19:52:16 +0900

    [학습데이터 실습] 요구사항1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -10,4 +10,16 @@ public class StringTest {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
     }
+
+    @Test
+    void split() {
+        //given
+        String data ="1,2";
+        //when
+        String[] split = data.split(",");
+        //then
+        assertThat(split).contains("2","1"); // 순서를 상관하지않음
+        assertThat(split).containsExactly("1","2"); // 순서도 확인함
+    }
+
 }
