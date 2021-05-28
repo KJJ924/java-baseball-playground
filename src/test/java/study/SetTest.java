7cd07bf474157c766ddd7a46b03df28d4af84199
jaejoon <dkansk924@naver.com>
2021-05-28 20:09:17 +0900

    [학습데이터 실습] setTest 요구사항2

--- src/test/java/study/SetTest.java
+++ src/test/java/study/SetTest.java
@@ -4,15 +4,17 @@ import static org.assertj.core.api.Assertions.*;
 
 import java.util.HashSet;
 import java.util.Set;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 /**
  * @author dkansk924@naver.com
  * @since 2021/05/28
  */
 public class SetTest {
+
     private Set<Integer> numbers;
 
     @BeforeEach
@@ -30,5 +32,13 @@ public class SetTest {
         int size = numbers.size();
         //then
         assertThat(size).isEqualTo(3);
+    }
+
+
+    @ParameterizedTest
+    @ValueSource(ints = {1,2,3})
+    void contains(int param) {
+        //then
+        assertThat(numbers.contains(param)).isTrue();
     }
 }
