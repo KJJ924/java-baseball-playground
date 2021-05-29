e10fc413f3da639310c7be4c4f27dd665a2d4432
jaejoon <dkansk924@naver.com>
2021-05-29 17:32:23 +0900

    [feat] Game class

---
+++ src/main/java/study/Game.java
@@ -0,0 +1,14 @@
+package study;
+
+/**
+ * @author dkansk924@naver.com
+ * @since 2021/05/29
+ */
+public class Game {
+
+    public static void main(String[] args) {
+        BaseBallGame baseBallGame = new BaseBallGame();
+        baseBallGame.start();
+    }
+
+}
