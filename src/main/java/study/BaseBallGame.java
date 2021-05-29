ee300a7fc6fd0a19c93db5e80fe4dcd793dea980
jaejoon <dkansk924@naver.com>
2021-05-30 00:41:49 +0900

    [feat] BaseBallGame

--- src/main/java/study/BaseBallGame.java
+++ src/main/java/study/BaseBallGame.java
@@ -1,10 +1,10 @@
 package study;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
+import study.view.GameView;
 
 /**
  * @author dkansk924@naver.com
@@ -14,28 +14,27 @@ public class BaseBallGame {
 
     private final RandomNumbersCreator numbersCreator = new RandomNumbersCreator();
     private final Scanner scanner = new Scanner(System.in);
-    public void start() {
-        numbersCreator.create();
-        String userInput = userInput();
+    private boolean status = false;
 
-        scanner.close();
-    }
+    public boolean start() {
+        StrikeAndBallCheckService service = new StrikeAndBallCheckService(numbersCreator.create());
 
-
-    private String userInput(){
-        System.out.print(GameInfoMessage.INPUT.message);
-        List<String> stringList = InputDataRefining(scanner.next());
-        System.out.println(stringList);
-        return null;
-    }
-
-    public List<String>  InputDataRefining(String inputData){
-        String[] spaceRemove = inputData.trim().split("");
-        if(spaceRemove.length !=3){
-            throw new IllegalArgumentException("입력값 오류 다시 입력하세요");
+        while (!status) {
+            status = service.checking(userInput());
         }
-        return Arrays.stream(spaceRemove).collect(Collectors.toList());
-
+        return restarted();
     }
 
+    private boolean restarted(){
+        return scanner.nextInt() == 1;
+    }
+    private List<Integer> userInput() {
+        GameView.inputPrint();
+        return InputDataRefining(scanner.next());
+    }
+
+    public List<Integer> InputDataRefining(String inputData) {
+        String[] spaceRemove = inputData.trim().split("");
+        return Arrays.stream(spaceRemove).map(Integer::valueOf).collect(Collectors.toList());
+    }
 }
